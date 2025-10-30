1ff11ecaf4aaa4baaf541aa5a5b2d109adac29dc
Vishal Kubavat <vkubavat77@gmail.com>
2025-10-30 06:17:56 +0000

    loops discussion

---
+++ 05_iterations/four.js
@@ -0,0 +1,27 @@
+const myObject = {
+    js  : 'JavaScript',
+    py  : 'Python',
+    rb  : 'Ruby',
+    cpp : 'C++',
+}
+
+for (const key in myObject) {
+    // console.log(`${key} shortcut is for ${myObject[key]}`);   
+}
+
+const programming = ["js", "py", "rb", "cpp", "java"];
+
+for (const key in programming) {
+    // console.log(programming[key]);
+}
+
+// const map = new Map();
+// map.set('IN', "India");
+// map.set('US', "United States");
+// map.set('FR', "France");
+// map.set('IN', "India");
+
+// for (const key in map) {
+//     console.log(key);
+// }
+
